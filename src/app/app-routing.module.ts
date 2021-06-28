0240dfd21ebf81cd2d91aa926006e960c0b4575d
jdecelis <jdecelis@everis.com>
2021-06-28 00:37:01 -0400

    limpieza proyecto base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormDosComponent } from './components/form-dos/form-dos.component';
-import { FormTresComponent } from './components/form-tres/form-tres.component';
-import { FormUnoComponent } from './components/form-uno/form-uno.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
-    path: 'form-uno', component: FormUnoComponent
+    path: '', component: AppComponent
   },
-  {
-    path: 'form-dos', component: FormDosComponent
-  },
-  {
-    path: 'form-tres', component: FormTresComponent
-  },
-
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { FormUnoComponent } from './components/form-uno/form-uno.component';
-import { FormDosComponent } from './components/form-dos/form-dos.component';
-import { FormTresComponent } from './components/form-tres/form-tres.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
-    MenuComponent,
-    FormUnoComponent,
-    FormDosComponent,
-    FormTresComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
