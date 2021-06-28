dc48b8bf217a519f8f7f7138d8fa3c87f7b5217f
jdecelis <jdecelis@everis.com>
2021-06-28 14:30:15 -0400

    refactor paginator

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginatorComponent implements OnInit {
   pageNumber = 1;
-  aux:number =1;
+  currentPage: number = 1;
   pageSize = 5;
   lista: Array<string> = [
     'elemento 1',
@@ -35,43 +35,46 @@ export class PaginatorComponent implements OnInit {
     console.log(this.numberPages);
     this.onPage(this.lista, this.pageSize, this.pageNumber);
     this.onListPages();
-    console.log(this.pagination);
-    console.log(this.listPages);
-    console.log(this.listPages);
-
   }
 
-  /*   onPage(array: Array<any>, pageSize: number, pageNumber: number) { */
   onPage(lista: Array<any>, pageSize: number, pageNumber: number) {
     return lista.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
   }
+
   onListPages() {
-    for (let i =0; i < this.numberPages; i++) {
+    for (let i = 0; i < this.numberPages; i++) {
       this.listPages[i] = i;
     }
   }
+
   nextPage() {
-    if (this.numberPages > this.pageNumber) {
-      this.pageNumber++;
+    if (this.numberPages > this.currentPage) {
+      this.currentPage++;
     }
-    this.pagination = this.onPage(this.lista, this.pageSize, this.pageNumber);
-    console.log(this.pageNumber);
+    this.pagination = this.onPage(this.lista, this.pageSize, this.currentPage);
   }
+
   previusPage() {
-    if (this.pageNumber > 1) {
-      this.pageNumber--;
+    if (this.currentPage > 1) {
+      this.currentPage--;
     }
-    this.pagination = this.onPage(this.lista, this.pageSize, this.pageNumber);
+    this.pagination = this.onPage(this.lista, this.pageSize, this.currentPage);
     console.log(this.pageNumber);
   }
-  onPageNumber(aux:number){
-    this.onPage(this.lista, this.pageSize, aux);
-    console.log(aux);
-  }
-  addOne(event: Event){
-    this.aux=parseInt((<HTMLButtonElement>event.target).value);
-    console.log(this.aux);
-    this.pagination = this.onPage(this.lista, this.pageSize, this.aux);
+
+ /*  onPageNumber(event: Event) {
+    console.log((<HTMLButtonElement>event.target).value);
+    this.pageNumber = parseInt((<HTMLButtonElement>event.target).value);
+    console.log(this.pageNumber);
+    this.onPage(this.lista, this.pageSize, this.pageNumber);
+    console.log(this.pageNumber);
+  } */
+
+  onPageNumber(event: Event) {
+    this.currentPage = parseInt((<HTMLButtonElement>event.target).value);
+    console.log(this.currentPage);
+    console.log(this.pageNumber);
+    this.pagination = this.onPage(this.lista, this.pageSize, this.currentPage);
   }
   pagination = this.onPage(this.lista, this.pageSize, this.pageNumber);
 }
